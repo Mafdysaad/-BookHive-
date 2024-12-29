2633b6290f87265f9f50c263e173db50bd1ccf85
mafdysaad <mafdysaad366@gmail.com>
2024-12-29 10:36:35 +0200

    refactor the code

--- lib/Features/home/presentation/views/book_details_view.dart
+++ lib/Features/home/presentation/views/book_details_view.dart
@@ -1,5 +1,7 @@
 import 'package:booklyapp/Features/home/presentation/views/widget/book_details_view_body.dart';
 import 'package:booklyapp/Features/home/presentation/views/widget/custom_Avatar.dart';
+import 'package:booklyapp/Features/home/presentation/views/widget/custom_actions_book_details.dart';
+import 'package:booklyapp/Features/home/presentation/views/widget/custom_leading_details_view.dart';
 
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:booklyapp/core/utils/fontstyle.dart';
@@ -21,49 +23,8 @@ class BookeDetailsView extends StatelessWidget {
         toolbarHeight: 90,
         leadingWidth: double.infinity,
         backgroundColor: const Color(0xFF7968FF),
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 20),
-          child: Row(
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.arrow_back_ios,
-                  color: Colors.white,
-                  size: 35,
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: CustomAvatar(),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Row(
-                children: [
-                  Text(
-                    'Johnathan',
-                    style: Fontstyle.proxima_Bold_19.copyWith(fontSize: 22),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Text('📚🎉')
-                ],
-              )
-            ],
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: SvgPicture.asset(
-              Assets.resourceImagesLogout,
-              height: 50,
-            ),
-          )
-        ],
+        leading: const CustomLeadingBookDetails(),
+        actions: const [CustomActionsBookDetails()],
       ),
       body: const BookDetailsViewBody(),
     );
---
+++ lib/Features/home/presentation/views/widget/custom_leading_details_view.dart
@@ -0,0 +1,45 @@
+import 'package:booklyapp/Features/home/presentation/views/widget/custom_Avatar.dart';
+import 'package:booklyapp/core/utils/fontstyle.dart';
+import 'package:flutter/material.dart';
+
+class CustomLeadingDetailsView extends StatelessWidget {
+  const CustomLeadingDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+              size: 35,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: CustomAvatar(),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Row(
+            children: [
+              Text(
+                'Johnathan',
+                style: Fontstyle.proxima_Bold_19.copyWith(fontSize: 22),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              const Text('📚🎉')
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
