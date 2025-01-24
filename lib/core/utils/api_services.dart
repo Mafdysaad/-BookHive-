74fbc84f1bdc4ef16a7682663ad0a5e9f7f785a1
mafdysaad <mafdysaad366@gmail.com>
2025-01-24 10:32:36 +0200

    create service locator and used it

--- lib/core/utils/api_services.dart
+++ lib/core/utils/api_services.dart
@@ -3,7 +3,10 @@ import 'package:dio/dio.dart';
 class Apiservices {
   final String BasURL = 'https://www.googleapis.com/books/v1/';
   final Dio _dio;
-  Apiservices(this._dio);
+
+  Apiservices(
+    this._dio,
+  );
   Future<Map<String, dynamic>> get(endpoint) async {
     Response respons = await _dio.get('$BasURL$endpoint');
     return respons.data;
---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,11 @@
+import 'package:booklyapp/Features/home/Data/repo/homerepoimplmantion.dart';
+import 'package:booklyapp/core/utils/api_services.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getit = GetIt.instance;
+
+void setup() {
+  getit.registerSingleton(Apiservices(Dio()));
+  getit.registerSingleton(Homerepoimplmantion(getit.get<Apiservices>()));
+}
